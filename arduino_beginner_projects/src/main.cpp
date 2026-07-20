767844284210792ad63270644ab5517817d78a58
mpeschek <michaelpeschek@icloud.com>
2026-07-20 18:45:27 +0200

    feat: alternating LED outputs

--- arduino_beginner_projects/src/main.cpp
+++ arduino_beginner_projects/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int switch_state;
+int red_state = 1;
+int blue_state = 0;
+void alternate(int& pin_1, int& pin_2);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(3, OUTPUT);     // green light
+  pinMode(4, OUTPUT);     // blue light 
+  pinMode(10, OUTPUT);     // red light 
+  pinMode(2, INPUT);      // Switch
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switch_state = digitalRead(2);
+
+  if (switch_state == LOW) {
+    // Only green light if button not pressed
+    digitalWrite(3, HIGH);
+
+    digitalWrite(4, LOW);
+    digitalWrite(10, LOW);
+  } else {
+
+    digitalWrite(3, LOW);
+
+    // alternating blue and red
+    digitalWrite(4, blue_state);
+    digitalWrite(10, red_state);
+    delay(200);
+    alternate(red_state, blue_state);
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void alternate(int& pin_1, int& pin_2) {
+  int intermediate = pin_1;
+  pin_1 = pin_2;
+  pin_2 = intermediate;
 }
